b6466e3d430a60a597d8f696635c9ca069113053
Yingli Sun <yingli64.sun@gmail.com>
2012-11-06 10:14:27 +0800

    Initial Commit

---
+++ MyGit/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MyGit
+//
+//  Created by  孙 英力 on 12-11-6.
+//  Copyright (c) 2012年  孙 英力. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
